a1e7c91035d7c1642e30c693539c66b2f99f7ec3
Sayedmahdi Raghib <88043053+mahdi-raghib@users.noreply.github.com>
2022-04-13 15:26:22 +0200

    added to repo

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 from flask import Flask, jsonify
 import time
 from rocketchat.api import RocketChatAPI
+from rocketchat.calls.chat.send_message import SendMessage
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
 
-    api = RocketChatAPI(settings={'username': 'someuser', 'password': 'somepassword',
-    'domain': 'https://myrockethchatdomain.com'})
+    api = RocketChatAPI(settings={'username': 'hello-world-bot', 'password': '12345678',
+    'domain': 'https://chat.milki-psy.dbis.rwth-aachen.de/home'})
 
-
-    return jsonify({"Time of Call": time.time()})
+    api.send_message('Hello world!', 1111)
+    #return jsonify({"Time of Call": time.time()})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
