24238fd3d958a2b1394571cf548f89652f9f575b
Sayedmahdi Raghib <88043053+mahdi-raghib@users.noreply.github.com>
2022-04-19 12:12:09 +0200

    added flaskenv file

---
+++ .flaskenv
@@ -0,0 +1,5 @@
+FLASK_ENV=development
+FLASK_APP=app.py
+ROCKETCHAT_URL="https://chat.milki-psy.dbis.rwth-aachen.de/home"
+ROCKETCHAT_USER= 'hello-world-bot'
+ROCKETCHAT_PASSWORD = '12345678'
--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, jsonify
-from rocketchat.api import RocketChatAPI
-#import time
+from rocketchat.api import RocketChatAPI  
 #from rocketchat.calls.chat.send_message import SendMessage
 #from rocketchat_API.rocketchat import RocketChat
 #from pprint import pprint
@@ -13,10 +12,8 @@ def hello():
     api = RocketChatAPI(settings={'username': 'hello-world-bot', 'password': '12345678',
     'domain': 'https://chat.milki-psy.dbis.rwth-aachen.de/home'})
     
-    api.get_private_rooms()
+    return api.get_private_rooms()
 
     #api.send_message('Hello world!', 11111)
     #return jsonify({"Time of Call": time.time()})
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
